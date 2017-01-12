ae4933fa74e229543e4d137e2300bf1ef84e1378
TimiYang <Uiryzd@163.com>
2017-01-12 23:40:24 +0800

    Fix

---
+++ data/posts-data.js
@@ -0,0 +1,47 @@
+var local_database = [
+    {
+        date: "Sep 18 2016",
+        title: "正是虾肥蟹壮时",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/1.png",
+        content: "菊黄蟹正肥，品尝秋之味。徐志摩把，“看初花的狄路”和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
+        reading: "112",
+        collection: "96",
+    }, {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/bl.png",
+        avatar: "/images/avatar/2.png",
+        content: "菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
+        reading: "112",
+        collection: "96",
+    }, {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/cat.png",
+        avatar: "/images/avatar/3.png",
+        content: "菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
+        reading: "112",
+        collection: "96",
+    }, {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/sls.jpg",
+        avatar: "/images/avatar/3.png",
+        content: "菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
+        reading: "112",
+        collection: "96",
+    }, {
+        date: "Nov 25 2016",
+        title: "比利·林恩的中场故事",
+        imgSrc: "/images/post/vr.png",
+        avatar: "/images/avatar/3.png",
+        content: "菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
+        reading: "112",
+        collection: "96",
+    }
+];
+
+module.exports = {
+    postLists:local_database
+}
--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,55 +1,15 @@
+var postsData = require('../../data/posts-data.js')
+
 Page({
   data:{
     posts_content:[]
   },
   onLoad:function(options){
-      var posts_content = [
-          {
-              date:"Sep 18 2016",
-              title:"正是虾肥蟹壮时",
-              post_img:"/images/post/crab.png",
-              author_img:"/images/avatar/1.png",
-              content:"菊黄蟹正肥，品尝秋之味。徐志摩把，“看初花的狄路”和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
-              view_num:"112",
-              collect_num:"96",
-          },{
-              date:"Nov 25 2016",
-              title:"比利·林恩的中场故事",
-              post_img:"/images/post/bl.png",
-              author_img:"/images/avatar/2.png",
-              content:"菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
-              view_num:"112",
-              collect_num:"96",
-          },{
-              date:"Nov 25 2016",
-              title:"比利·林恩的中场故事",
-              post_img:"/images/post/cat.png",
-              author_img:"/images/avatar/3.png",
-              content:"菊黄蟹正肥肥，品尝秋之味。徐志摩把把，“看初花的狄路”和和“到楼外楼吃蟹”，并列为秋天来杭州不能错过的事情。",
-              view_num:"112",
-              collect_num:"96",
-          }
-      ];
+      
       this.setData({
-        posts_content:posts_content
+        posts_content:postsData.postLists
       })
 
-  },
-  onReady:function(){
-    // 生命周期函数--监听页面初次渲染完成
-    console.log("onReady")
-  },
-  onShow:function(){
-    // 生命周期函数--监听页面显示
-    console.log("onShow")
-  },
-  onHide:function(){
-    // 生命周期函数--监听页面隐藏
-    console.log("onHide")
-  },
-  onUnload:function(){
-    // 生命周期函数--监听页面卸载
-    console.log("onUnload")
   },
   onPullDownRefresh: function() {
     // 页面相关事件处理函数--监听用户下拉动作
